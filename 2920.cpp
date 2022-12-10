4206e6477a99b9e58c0e8f82c758a70d3dfae1df
junyojeo <jjy8479@gmail.com>
2022-12-11 03:16:30 +0900

    1

--- 2920.cpp
+++ 2920.cpp
@@ -2,27 +2,40 @@
 #define fast_io ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 using namespace std;
 
-void	solve()
+int	solve()
 {
-	int a[8];
-	for (int i=0;i<8;i++)
-		cin >> a[i];
+	int arr[8], a[8], ascending = 0, descending = 0, j = 7;
 	for (int i = 0; i < 8; i++)
-	{
-		if ((a[0] == 1 && a[i] == a[i + 1] - 1) || (a[0] == 8 && a[i] == a[i + 1] + 1))
-			continue;
-		cout << "mixed";
+		arr[i] = i + 1;
+	cin >> a[0];
+	if (a[0] == arr[0])
+		ascending = 1;
+	else if (a[0] == arr[7])
+		descending = 1;
+	else
 		return (0);
+	for (int i = 1; i < 8; i++)
+	{
+		cin >> a[i];
+		--j;
+		if (ascending && a[i] == arr[i])
+			continue;
+		else if (descending && a[i] == arr[j])
+			continue;
+		else
+			return (0);
 	}
-	if (a[0] == 1)
-		cout << "asending";
-	if (a[0] == 8)
-		cout << "desending";
+	if (ascending)
+		cout << "ascending";
+	else if (descending)
+		cout << "descending";
+	return (1);
 }
 
 int	main(void)
 {
 	fast_io;
-	solve();	
+	if (!solve())
+		cout << "mixed";	
 	return (0);
 }
