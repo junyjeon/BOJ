e65a07daef89d8de139028ef423d5606f9204a14
junyojeo <jjy8479@gmail.com>
2023-03-12 22:46:53 +0900

    그대로 출력하기

---
+++ sprout/27866.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define fast_io ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+void solve()
+{
+	string line;
+	while (getline(cin, line))
+		cout << line << "\n";
+}
+
+int main(void)
+{
+	fast_io;
+	solve();
+	return 0;
+}
