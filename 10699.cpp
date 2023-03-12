94e95ce7909f317e4e26f14dfc2bcdd6b282cb56
junyojeo <jjy8479@gmail.com>
2023-03-12 22:34:18 +0900

    학점계산

--- 10699.cpp
+++ 10699.cpp
@@ -4,21 +4,32 @@ using namespace std;
 
 void solve()
 {
-	string str;
-	cin >> str;
-	for (int i = 0; i < str.length(); i++)
+	map<string, double> grades = 
 	{
-		if (str[i] < 'a')
-			str[i] = tolower(str[i]);
-		else
-			str[i] = toupper(str[i]);
-	}
-	cout << str;
+		{"A+", 4.3},
+		{"A0", 4.0},
+		{"A-", 3.7},
+		{"B+", 3.3},
+		{"B0", 3.0},
+		{"B-", 2.7},
+		{"C+", 2.3},
+		{"C0", 2.0},
+		{"C-", 1.7},
+		{"D+", 1.3},
+		{"D0", 1.0},
+		{"D-", 0.7},
+		{"F", 0.0},
+	};
+	string grade;
+	cin >> grade;
+	cout.precision(1);
+	cout << fixed << grades[grade];
+
 }
 
 int main(void)
 {
 	fast_io;
 	solve();
-	return 0;
+	return (0);
 }
