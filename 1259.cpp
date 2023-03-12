bd968fb55b9c0f085c9d532c0518afcd1841f828
junyojeo <jjy8479@gmail.com>
2023-03-12 18:50:27 +0900

    랜선 자르기

--- 1259.cpp
+++ 1259.cpp
@@ -11,9 +11,7 @@ void solve()
 
 	for (int i = 0; i < k; i++)
 		cin >> cables[i];
-	int min_length = *min_element(cables, cables + k);
-
-	long long left = 1, right = min_length;
+	long long left = 1, right = *max_element(cables, cables + k);
 	while (left <= right)
 	{
 		long long mid = (left + right) / 2;
@@ -25,7 +23,7 @@ void solve()
 		else
 			right = mid - 1;
 	}
-	cout << right << "\n";
+	cout << right;
 }
 
 int main(void)
